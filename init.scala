da9b4701a318b4a32ba129ee21085b4ad2a32831
tdelbecque <git@sodad.com>
2018-06-25 11:42:00 +0200

    more imports

--- init.scala
+++ init.scala
@@ -1,6 +1,8 @@
 import scala.language.postfixOps
 import com.sodad.abstracts._
 import com.sodad.abstracts.Control._
+import com.sodad.abstracts.Aliases._
+import com.sodad.abstracts.Application.implicits._
 
 implicit val session = spark
 
